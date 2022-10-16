a8e2bf6cfc9cce1df55a981cb930b18e00a18f64
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2022-10-16 16:30:02 +0530

    Time: 27 ms (19.28%), Space: 54.3 MB (48.21%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int totalHammingDistance(int[] A) {
+        long res=0;
+        for(int i=0;i<32;i++)
+        {
+            long countOne=0;
+            for(int j=0;j<A.length;j++)
+            {
+                if(((A[j]>>i)&1)==1)countOne++;
+                
+            }
+            res= (res+(countOne* (A.length-countOne)))%1000000007;
+        }
+        return (int)(res);
+        
+    }
+}
