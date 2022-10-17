f36ccbdd72aad06b2b5fa0e91622c0dc21ff7871
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2022-10-18 00:50:31 +0530

    Time: 8 ms (20.96%), Space: 5.9 MB (50.86%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,17 +2,15 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t a) {
         
-         uint32_t rev=0;
-        
+        uint32_t rev=0;
         uint32_t i=0;
         while(i<32)
         {
             uint32_t rem= a%2;
-            a>>=1;
-            rev= rev<<1;
+            rev<<= 1;
             rev+=rem;
             i++;
-            //System.out.println(Long.toBinaryString(rev));
+            a>>=1;
         }
         return rev;
     }
