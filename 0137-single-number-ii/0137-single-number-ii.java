53700dcd8c74dc558ae732e2a60ea74adf4c1b0a
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2022-10-20 20:45:41 +0530

    Time: 11 ms (30.17%), Space: 43.5 MB (77.10%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] A) {
+        int num=0;
+        for(int i=0;i<32;i++)
+        {
+            int one=0;
+            for(int j=0; j<A.length;j++)
+            {
+                one+=(A[j]&1);
+                A[j]>>=1;
+            }
+            int n=one%3;
+            num= num| n<<i ;
+        }
+                            //System.out.print(Integer.toBinaryString(-4));
+
+        return num;
+    }
+}
