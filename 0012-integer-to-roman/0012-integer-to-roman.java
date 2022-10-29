f9906ed0960b7fac761172359a27361ba0c6eeac
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2022-10-29 17:04:10 +0530

    Time: 4 ms (99.89%), Space: 41.8 MB (96.50%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -2,85 +2,85 @@ class Solution {
     public String intToRoman(int A) {
         
         
-         String res="";
+        StringBuilder res=new StringBuilder();
         while(A>=1000)
         {
-            res+="M";
+            res.append("M");
             A-=1000;
         }
         
         if(A>=900)
         {
-            res+="CM";
+            res.append("CM");
             A-=900;
         }
         
         if(A>=500)
         {
-            res+="D";
+            res.append("D");
             A-=500;
         }
         
         if(A>=400)
         {
-            res+="CD";
+            res.append("CD");
             A-=400;
         }
         
         while(A>=100)
         {
-            res+="C";
+            res.append("C");
             A-=100;
         }
         
         if(A>=90)
         {
-            res+="XC";
+            res.append("XC");
             A-=90;
         }
         
         if(A>=50)
         {
-            res+="L";
+            res.append("L");
             A-=50;
         }
         
         if(A>=40)
         {
-            res+="XL";
+            res.append("XL");
             A-=40;
         }
         
         while(A>=10)
         {
-            res+="X";
+            res.append("X");
             A-=10;
         }
         
         if(A>=9)
         {
-            res+="IX";
+            res.append("IX");
             A-=9;
         }
         
         if(A>=5)
         {
-            res+="V";
+            res.append("V");
             A-=5;
         }
         
         if(A>=4)
         {
-            res+="IV";
+            res.append("IV");
             A-=4;
         }
         
         while(A>=1)
         {
-            res+="I";
+            res.append("I");
             A-=1;
         }
-        return res;
+        return res.toString();
         
     }
 }
