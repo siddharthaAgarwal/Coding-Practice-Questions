a3cec7067ad799a5c3372c9964e0c621545222c1
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2024-02-25 21:28:06 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (92.44%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -2,12 +2,8 @@ class Solution {
     
     
     public int numberOfMatches(int n) {
-        
-        if(n<=1)
-        {
+        if(n<=1) 
             return 0;
-        }
-        
         return n/2+numberOfMatches((n+1)/2);
     }
 }
