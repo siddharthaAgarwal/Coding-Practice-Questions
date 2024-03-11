c70d33433def4ed438e13ad5dffce82e6c11c360
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2024-03-11 20:44:15 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (7.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,15 +1,20 @@
 class Solution {
-    public int fib(int n) {
-       int[] arr = new int[n+1];
-      if(n<=1)
-      return n;
-       arr[0]=0;
-       arr[1]=1;
+    public int fib(int number) {
+    {
+        int[] dp = new int[number+1];
+        
+        dp[0]=0;
+        if(number>=1)
+        {
+            dp[1]=1;
+        }
+        
+        for(int i=2;i<=number;i++)
+        {
+            dp[i]= dp[i-1]+dp[i-2];
+        }
+        return dp[number];
+    }
 
-       for(int i=2;i<=n;i++)
-       {
-           arr[i] = arr[i-2]+arr[i-1];
-       }
-       return arr[n];
     }
 }
