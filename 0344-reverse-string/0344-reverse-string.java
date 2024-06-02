4dac6395d772cde437116bb98240d1e484283456
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2024-06-02 15:46:06 +0530

    Time: 0 ms (100%), Space: 45.5 MB (87.32%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void reverseString(char[] s) {
+        for(int i=0;i<s.length/2;i++)
+        {
+            char c= s[s.length-i-1];
+            s[s.length-i-1] = s[i];
+            s[i]=c;
+        }
+    }
+}
