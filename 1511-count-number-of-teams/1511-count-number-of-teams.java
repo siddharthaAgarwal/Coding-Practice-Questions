f60985416b16cce1daa64d8ebc5b9332dce65c37
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2024-07-29 21:11:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.java
+++ 1511-count-number-of-teams/1511-count-number-of-teams.java
@@ -1,21 +1,18 @@
 class Solution {
-    public int numTeams(int[] arr) {
-        int n = arr.length;
-        int ans = 0;
-        for (int i = 1; i < n - 1; i++) {
-            int l[] = {0, 0}, r[] = {0, 0};
-            for (int j = 0; j < i; j++) {
-                l[arr[j] < arr[i] ? 0 : 1]++;
+    class Solution {
+        public int numTeams(int[] rating) {
+            int n1 = 0;
+            for (int i = 0; i < rating.length - 2; i++) {
+                for (int j = i + 1; j < rating.length - 1; j++) {
+                    for (int k = j + 1; k < rating.length; k++) {
+                        if ((rating[i] < rating[j] && rating[j] < rating[k])
+                                || (rating[i] > rating[j] && rating[j] > rating[k])) {
+                            n1++;
+                        }
+                    }
+                }
             }
-            for (int k = i + 1; k < n; k++) {
-                r[arr[k] < arr[i] ? 0 : 1]++;
-            }
-
-            System.out.println(Arrays.toString(l));
-            System.out.println(Arrays.toString(r));
-        
-            ans += l[0] * r[1] + l[1] * r[0];
+            return n1;
         }
-        return ans;
     }
 }
