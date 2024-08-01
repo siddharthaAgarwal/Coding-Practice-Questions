b10f6fa99a2669058befd66834468b7562ca59cc
Siddhartha Agarwal <48640732+siddharthaAgarwal@users.noreply.github.com>
2024-08-01 11:09:49 +0530

    Time: 0 ms (100%), Space: 42.7 MB (27.08%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.java
@@ -2,11 +2,14 @@ class Solution {
     public int countSeniors(String[] details) {
         
         int res=0;
-        //System.out.println(details[0].substring(11,13));
         for(String s: details)
         {
-            if(s.substring(11,13).compareTo("60")>0)
-            res++;
+            
+            if(s.charAt(11)>='7'||(s.charAt(11)=='6' && s.charAt(12)>'0'))
+            {
+                res++;
+            }
+            
         }
         return res;
     }
